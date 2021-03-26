bfcc231c9a1723dd44a40dd44e526737b028c105
jamiesun <jamiesun.net@gmail.com>
2021-03-26 17:13:47 +0800

    ignore

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/EdgeComputingTerminal/Monibuca-jessica
 go 1.16
 
 require (
-	github.com/Monibuca/engine/v3 v3.0.0-alpha7
-	github.com/Monibuca/utils/v3 v3.0.0-alpha5
-	github.com/gobwas/httphead v0.1.0 // indirect
-	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.0.4
-	github.com/logrusorgru/aurora v2.0.3+incompatible
+	github.com/Monibuca/engine/v2 v2.4.0
+	github.com/Monibuca/utils v1.0.0
+	github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee // indirect
+	github.com/gobwas/pool v0.2.0 // indirect
+	github.com/gobwas/ws v1.0.2
+	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
 )
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package jessica
 
 import (
+	"embed"
+	"mime"
 	"net/http"
+	"path"
+	"strings"
 
-	. "github.com/Monibuca/engine/v3"
-	"github.com/Monibuca/utils/v3"
+	. "github.com/Monibuca/engine/v2"
+	"github.com/Monibuca/utils"
 	. "github.com/logrusorgru/aurora"
 )
 
@@ -15,20 +19,42 @@ var config struct {
 	ListenAddrTLS string
 }
 
+//go:embed ui/*
+//go:embed README.md
+var ui embed.FS
+
+var publicPath string
+
 func init() {
 	plugin := &PluginConfig{
 		Name:   "Jessica",
+		Type:   PLUGIN_SUBSCRIBER,
 		Config: &config,
 		Run:    run,
+		UIFile: &ui,
 	}
 	InstallPlugin(plugin)
 }
 func run() {
+	http.HandleFunc("/jessibuca/", jessibuca)
 	if config.ListenAddr != "" || config.ListenAddrTLS != "" {
-		utils.Print(Green("Jessica start at"), BrightBlue(config.ListenAddr), BrightBlue(config.ListenAddrTLS))
+		Print(Green("Jessica start at"), BrightBlue(config.ListenAddr), BrightBlue(config.ListenAddrTLS))
 		utils.ListenAddrs(config.ListenAddr, config.ListenAddrTLS, config.CertFile, config.KeyFile, http.HandlerFunc(WsHandler))
 	} else {
-		utils.Print(Green("Jessica start reuse gateway port"))
+		Print(Green("Jessica start reuse gateway port"))
 		http.HandleFunc("/jessica/", WsHandler)
+	}
+}
+func jessibuca(w http.ResponseWriter, r *http.Request) {
+	filePath := strings.TrimPrefix(r.URL.Path, "/jessibuca")
+	if mime := mime.TypeByExtension(path.Ext(filePath)); mime != "" {
+		w.Header().Set("Content-Type", mime)
+	}
+	if f, err := ui.ReadFile("ui/public" + filePath); err == nil {
+		if _, err = w.Write(f); err != nil {
+			w.WriteHeader(500)
+		}
+	} else {
+		w.WriteHeader(404)
 	}
 }
